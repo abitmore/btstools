2a06f113f7027569791f4b99f40b0fc498ebdeb1
Abit <abitmore@users.noreply.github.com>
2020-12-02 11:17:20 +0100

    Add code to check max_ops_per_account
    And other minor changes:
    - Rename variables
    - Remove unused code

--- check_bts_api_nodes.sh
+++ check_bts_api_nodes.sh
@@ -1,45 +1,30 @@
 #!/bin/sh
 
 api_nodes_file=https://raw.githubusercontent.com/bitshares/bitshares-ui/develop/app/api/apiConfig.js
-rpc_query='{"method":"call","id":1,"jsonrpc":"2.0","params":["database","get_dynamic_global_properties",[]]}'
-rpc_query2='{"method":"call","id":1,"jsonrpc":"2.0","params":["database","get_chain_properties",[]]}'
+dgprops_query='{"method":"call","id":1,"jsonrpc":"2.0","params":["database","get_dynamic_global_properties",[]]}'
+cprops_query='{"method":"call","id":1,"jsonrpc":"2.0","params":["database","get_chain_properties",[]]}'
+acstats_query='{"method":"call","id":1,"jsonrpc":"2.0","params":["database","get_objects",[["2.6.33015"]]]}'
 
 api_nodes=`curl "$api_nodes_file" 2>/dev/null | grep -E "^( )*url" | grep -v fake | cut -f2 -d '"' | grep '^wss://' | cut -c5-`
 
 for node in $api_nodes; do
   # //api.bts.mobi/ws
   printf "%-35s" "wss:$node"
-  head_time=`curl --connect-timeout 10 -d "$rpc_query" https:$node 2>/dev/null |jq -M .|grep '"time"'|cut -f4 -d'"'`
+  head_time=`curl --connect-timeout 10 -d "$dgprops_query" https:$node 2>/dev/null |jq -M . 2>/dev/null|grep '"time"'|cut -f4 -d'"'`
   if [ -n "$head_time" ]; then
     head_age=`expr $(date +%s --utc) - $(date +%s --utc -d "$head_time")`
-    chain_id=`curl --connect-timeout 10 -d "$rpc_query2" https:$node 2>/dev/null |jq -M .|grep '"chain_id"'|cut -f4 -d'"'`
-    echo "head age $head_age \t chain_id [$chain_id]"
+    chain_id=`curl --connect-timeout 10 -d "$cprops_query" https:$node 2>/dev/null |jq -M .|grep '"chain_id"'|cut -f4 -d'"'`
+    echo -n "head age $head_age s\t chain_id [$chain_id]"
+    if [ "x$chain_id" = "x4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8" ]; then # BitShares Mainnet
+      stats=`curl --connect-timeout 10 -d "$acstats_query" https:$node 2>/dev/null |jq -M .`
+      total_ops=`echo "$stats" |grep '"total_ops"'|awk '{print $2}'|cut -f1 -d','`
+      removed_ops=`echo "$stats" |grep '"removed_ops"'|awk '{print $2}'|cut -f1 -d','`
+      his_ops=$(($total_ops - $removed_ops))
+      echo "\tmax_ops_per_account $his_ops"
+    else
+      echo
+    fi
   else
     echo "Down"
   fi
 done
-
-for seed in ""; do
-  exit
-#for seed in ${seeds}; do
-  seed_host=`echo $seed|cut -f1 -d':'`;
-  seed_port=`echo $seed|cut -f2 -d':'`;
-
-  seed_ips=`dig "$seed_host"|grep -v '^;'|grep -E "IN\s*A"|awk '{print $5}'|sort`
-  count=`echo "$seed_ips"|wc -l`
-  if [ "$count" = "0" ]; then
-    echo "$seed\tDNS lookup failed"
-  else
-    if [ "$count" = "1" ]; then
-      printf "%-32s" $seed
-    else
-      printf "%s (%d IP addresses)\n" $seed $count
-    fi
-    for seed_ip in $seed_ips; do
-      echo -n "${seed_ip}:${seed_port}\t";
-      [ -n "`echo EOF | nc ${seed_ip} $seed_port -w 10 -q 2`" ] && echo Ok || echo Failed
-    done
-  fi
-
-done
-
