6492a50833e45b61c09bdd5d5947242fd932db4a
Abit <abitmore@users.noreply.github.com>
2021-10-05 20:25:12 +0200

    Add timeout

--- check_bts_api_nodes.sh
+++ check_bts_api_nodes.sh
@@ -4,6 +4,8 @@
 # - curl
 # - jq
 
+CURL="curl --connect-timeout 10 --max-time 20"
+
 api_nodes_file=https://raw.githubusercontent.com/bitshares/bitshares-ui/develop/app/api/apiConfig.js
 dgprops_query='{"method":"call","id":1,"jsonrpc":"2.0","params":["database","get_dynamic_global_properties",[]]}'
 cprops_query='{"method":"call","id":1,"jsonrpc":"2.0","params":["database","get_chain_properties",[]]}'
@@ -12,15 +14,15 @@ testnet_acstats_query='{"method":"call","id":1,"jsonrpc":"2.0","params":["databa
 mainnet_chain_id="4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8"
 testnet_chain_id="39f5e2ede1f8bc1a3a54a7914414e3779e33193f1f5693510e73cb7a87617447"
 
-api_nodes=`curl "$api_nodes_file" 2>/dev/null | grep -E "^( )*url" | grep -v fake | cut -f2 -d '"' | grep '^wss://' | cut -c5-`
+api_nodes=`$CURL "$api_nodes_file" 2>/dev/null | grep -E "^( )*url" | grep -v fake | cut -f2 -d '"' | grep '^wss://' | cut -c5-`
 
 for node in $api_nodes; do
   # //api.bts.mobi/ws
   printf "%-35s" "wss:$node"
-  head_time=`curl --connect-timeout 10 -d "$dgprops_query" https:$node 2>/dev/null |jq -M . 2>/dev/null|grep '"time"'|cut -f4 -d'"'`
+  head_time=`$CURL -d "$dgprops_query" https:$node 2>/dev/null |jq -M . 2>/dev/null|grep '"time"'|cut -f4 -d'"'`
   if [ -n "$head_time" ]; then
     head_age=`expr $(date +%s --utc) - $(date +%s --utc -d "$head_time")`
-    chain_id=`curl --connect-timeout 10 -d "$cprops_query" https:$node 2>/dev/null |jq -M .|grep '"chain_id"'|cut -f4 -d'"'`
+    chain_id=`$CURL -d "$cprops_query" https:$node 2>/dev/null |jq -M .|grep '"chain_id"'|cut -f4 -d'"'`
     echo -n "head age $head_age s\t chain_id [$chain_id]"
     if [ "x$chain_id" = "x$mainnet_chain_id" -o "x$chain_id" = "x$testnet_chain_id" ]; then # BitShares Mainnet or Testnet
       if [ "x$chain_id" = "x$mainnet_chain_id" ]; then # mainnet
@@ -28,7 +30,7 @@ for node in $api_nodes; do
       else
         acstats_query=$testnet_acstats_query
       fi
-      stats=`curl --connect-timeout 10 -d "$acstats_query" https:$node 2>/dev/null |jq -M .`
+      stats=`$CURL -d "$acstats_query" https:$node 2>/dev/null |jq -M .`
       total_ops=`echo "$stats" |grep '"total_ops"'|awk '{print $2}'|cut -f1 -d','`
       removed_ops=`echo "$stats" |grep '"removed_ops"'|awk '{print $2}'|cut -f1 -d','`
       if [ -n "$total_ops" -a -n "$removed_ops" ]; then
