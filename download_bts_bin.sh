813cb59b4973159194564e9d1793b224b7d2a339
Abit <abitmore@users.noreply.github.com>
2022-01-25 15:20:20 +0100

    Add argument to download a specific version
    To download a specific version, provide it as the first argument.
    Example:
      ./download_bts_bin.sh 6.0.1

--- download_bts_bin.sh
+++ download_bts_bin.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-VERSION=`curl https://github.com/bitshares/bitshares-core/releases/latest 2>/dev/null | cut -f2 -d'"' | cut -f8 -d'/'`
+if [ -n "$1" ]; then
+   VERSION=$1
+else
+   VERSION=`curl https://github.com/bitshares/bitshares-core/releases/latest 2>/dev/null | cut -f2 -d'"' | cut -f8 -d'/'`
+fi
 DIR=bitshares-core-${VERSION}-linux-amd64-bin
 FILE=${DIR}.tar.bz2
 
@@ -13,4 +17,3 @@ tar xjf ${FILE}
 mv ${DIR}/witness_node ./witness_node.${VERSION}
 mv ${DIR}/cli_wallet ./cli_wallet.${VERSION}
 rm -rf ${DIR} ${FILE}
-
