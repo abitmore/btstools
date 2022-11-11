9944bd62cb33aa24b6a69d4288283ec16db6cc84
Abit <abitmore@users.noreply.github.com>
2022-11-12 00:20:36 +0100

    Fix seed file process and NUL byte issue, reformat

--- check_bts_seeds.sh
+++ check_bts_seeds.sh
@@ -11,7 +11,7 @@ else
   seeds_file=https://raw.githubusercontent.com/bitshares/bitshares-core/${BRANCH}/libraries/egenesis/seed-nodes.txt
 fi
 
-seeds=`curl $seeds_file 2>/dev/null | tail -n "+2" | cut -f2 -d'"'`
+seeds=`curl $seeds_file 2>/dev/null | grep -v '^//' | cut -f2 -d'"'`
 
 for seed in ${seeds}; do
   seed_host=`echo $seed|cut -f1 -d':'`;
@@ -19,17 +19,18 @@ for seed in ${seeds}; do
 
   seed_ips=`dig "$seed_host"|grep -v '^;'|grep -E "IN\s*A"|awk '{print $5}'|sort`
   if [ -z "$seed_ips" ]; then
-    echo "$seed\t\t\t\tDNS lookup failed"
+    printf "%-62s" $seed
+    echo "DNS lookup failed"
   else
     count=`echo "$seed_ips"|wc -l`
     if [ "$count" = "1" ]; then
-      printf "%-32s" $seed
+      printf "%-38s" $seed
     else
       printf "%s (%d IP addresses)\n" $seed $count
     fi
     for seed_ip in $seed_ips; do
       printf "%-24s" "${seed_ip}:${seed_port}"
-      [ -n "`nc ${seed_ip} $seed_port -w 10 -W 1`" ] && echo Ok || echo Failed
+      [[ $( nc ${seed_ip} $seed_port -w 10 -W 1 | wc -c ) -ne 0 ]] && echo Ok || echo Failed
     done
   fi
 
